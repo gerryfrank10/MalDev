5c3d64a72191d6e8217c380b1e17545982eea94b
medusa <gerryfrank720@gmail.com>
2022-03-10 06:47:05 +0300

    Added assemblies lookup

---
+++ LookUpFunction.ps1
@@ -0,0 +1,14 @@
+﻿function LooKUpFunc {
+
+    Param ($modulename, $functionname)
+    $assem = ([System.AppDomain]::CurrentDomain.GetAssemblies() | Where-Object {
+        $_.GlobalAssemblyCache -and $_.Location.Split('\\')[-1].Equals("System.dll") }).GetType("Microsoft.Win32.UnsafeNativeMethods")
+
+        $tmp = @()
+        $GetModuleHandle = $assem.GetMethod("GetModuleHandle")
+
+        $assem.GetMethods() | ForEach-Object { if ($_.Name -eq "GetProcAddress" ) {$tmp+=$_}}
+        return $tmp[0].Invoke($null, @($assem.GetMethod("GetModuleHandle").Invoke($null, @($modulename)), $functionname)) 
+}
+
+$MessageBoxA = LooKUpFunc user32.dll MessageBoxA
---
+++ assemblies.ps1
@@ -0,0 +1,13 @@
+﻿$Assemblies = [System.AppDomain]::CurrentDomain.GetAssemblies()
+
+$Assemblies | 
+    ForEach-Object {
+        $_.Location
+        $_.GetTypes() |
+            ForEach-Object {
+                $_ | Get-Member -Static | Where-Object {
+                    $_.TypeName.Equals("Microsoft.Win32.UnsafeNativeMethods")
+                    }
+                } 2> $null
+      }
+
